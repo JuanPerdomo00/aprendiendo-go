772b254285d41eb573fd593f597f20944bebf9f9
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2023-01-06 09:58:08 -0500

    operadores aritmeticos en go

---
+++ mathgoOperadoresAritmeticos/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 10
+	y := 50
+
+	// suma
+	result := x + y
+
+	fmt.Println("la suma es: ", result)
+
+	// resta
+	result = y - x
+	fmt.Println("la resta es: ", result)
+
+	// multiplicacion
+	result = x * y
+	fmt.Println("la multiplicacion es: ", result)
+
+	// divicion
+	result = y / x
+	fmt.Println("la divicion es: ", result)
+
+	// modulo
+	result = y % x
+	fmt.Println("modulo: ", result)
+}
